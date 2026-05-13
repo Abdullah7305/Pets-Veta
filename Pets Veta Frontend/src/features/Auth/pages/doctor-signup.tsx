ad602bc22db56d3afe8963ec7ec08fa90525ccda
Abdullah7305 <abdullahsuleman299@gmail.com>
2026-05-13 19:41:45 +0500

    Created Schema for Doctor Admin Pet Owner

---
+++ Pets Veta Frontend/src/features/Auth/pages/doctor-signup.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const DoctorSignup = () => {
+    return (
+        <div>
+            <h2>DoctorSignup</h2>
+        </div>
+    );
+};
+
+export default DoctorSignup;
---
+++ Pets Veta Frontend/src/features/Auth/pages/pet-owner-signup.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const PetOwnerSignup = () => {
+    return (
+        <div>
+            <h2>PetOwnerSignup</h2>
+        </div>
+    );
+};
+
+export default PetOwnerSignup;
