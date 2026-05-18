ff76b9aa0e1d5aff2cd1737c6226cad5e1a084e2
mtf25005165-ops <mtf25005165@ue.edu.pk>
2026-05-18 21:30:56 +0500

    doctor dashboard update

---
+++ Pets Veta Frontend/src/features/DoctorDashboard/components/DoctorProfileButton.tsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+
+type DoctorProfileButtonProps = {
+  name: string;
+  image?: string;
+};
+
+const DoctorProfileButton = ({ name, image }: DoctorProfileButtonProps) => {
+  const navigate = useNavigate();
+
+  const handleOpenProfile = () => {
+    navigate("/doctor/profile");
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleOpenProfile}
+      className="rounded-full transition hover:scale-105 focus:outline-none focus:ring-4 focus:ring-teal-100"
+      title="Open Profile"
+    >
+      {image ? (
+        <img
+          src={image}
+          alt={name}
+          className="h-12 w-12 rounded-full object-cover ring-4 ring-slate-100"
+        />
+      ) : (
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#F9C5A8] text-2xl ring-4 ring-slate-100">
+          👩‍⚕️
+        </div>
+      )}
+    </button>
+  );
+};
+
+export default DoctorProfileButton;
--- Pets Veta Frontend/src/routes/routes.tsx
+++ Pets Veta Frontend/src/routes/routes.tsx
@@ -2,42 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import LandingPageRoutes from "../features/Landing Page/routes";
 import { servicesRoutes } from "../features/Services";
 import Notfound from "../shared/components/Notfound/Notfound";
-
-import DoctorSignup from "../features/Auth/pages/doctor-signup";
-import LoginPage from "../features/Auth/pages/login";
-import VerifyOtpPage from "../features/Auth/pages/verify-otp";
-import ResetPasswordPage from "../features/Auth/pages/reset-password";
-import ForgotPasswordPage from "../features/Auth/pages/forgot-page";
-import OtpVerifyPage from "../features/Auth/pages/otp-verify";
+// import AuthRouter from "../features/Auth/auth.route";
+import { doctorDashboardRoutes } from "../features/DoctorDashboard/doctorDashboard.route";
 
 const Router = createBrowserRouter([
   ...servicesRoutes,
   ...LandingPageRoutes,
+  // ...AuthRouter,
+  ...doctorDashboardRoutes,
 
-  {
-    path: "/doctor-signup",
-    element: <DoctorSignup />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/forgot-password",
-    element: <ForgotPasswordPage />,
-  },
-  {
-    path: "/verify-otp",
-    element: <VerifyOtpPage />,
-  },
-  {
-    path: "/reset-password",
-    element: <ResetPasswordPage />,
-  },
-  {
-    path: "/otp-verify",
-    element: <OtpVerifyPage />,
-  },
   {
     path: "*",
     element: <Notfound />,
